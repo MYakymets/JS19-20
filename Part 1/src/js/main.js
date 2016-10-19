bdd3887210aad166ea3b4b99202671dfb541f930
yakimechik <max.yakimez@yandex.ru>
2016-10-19 11:56:39 +0200

    added JS19-20

---
+++ Part 1/src/js/main.js
@@ -0,0 +1,13 @@
+//= ../../node_modules/jquery/dist/jquery.min.js
+
+/*
+Custom
+ */
+//= partials/jquery.ui.widget.js
+//= partials/jquery.event.drag.js
+//= partials/jquery.rs.carousel.js
+//= partials/jquery.rs.carousel-autoscroll.js
+//= partials/jquery.rs.carousel-continuous.js
+//= partials/jquery.rs.carousel-touch.js
+
+//= partials/my-carousel.js
---
+++ Part 1/src/js/partials/my-carousel.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('.rs-carousel').carousel({
+    	itemsPerTransition: 1,
+    	nextPrevActions: false,
+    	pagination: true,
+    	speed: 'normal',
+    	easing: 'swing',
+    	autoScroll: true,
+    	pause: 8000,
+    	continuous: true
+    });
+
+});
